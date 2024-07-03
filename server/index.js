94906c534dd1f87e64cc77e08946c6130280f656
Kumar Shresth <kumarshresth2004@gmail.com>
2024-07-03 15:43:15 +0530

    #

--- server/index.js
+++ server/index.js
@@ -1,1 +1,18 @@
-console.log('hello')
+const express = require('express')
+const mongoose = require('mongoose')
+const cors = require('cors')
+const dotenv = require('dotenv')
+dotenv.config()
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+
+mongoose.connect(process.env.MONGO_URI)
+.then(()=>console.log('MongoDB connected'))
+.catch((err)=>console.log(err))
+
+app.use('/auth',require('./routes/authRoutes'))
+
+const PORT = process.env.PORT || 5000
+app.listen(PORT,()=>console.log(`Server is listening at ${PORT}`))
